19b161ffb7e95c27eff555ab95a2e63d435d42e7
Solovev Anton <solovev.anton93@gmail.com>
2023-07-19 18:44:37 +0200

    added address creation

--- src/main/java/com/solovev/model/PetryPot.java
+++ src/main/java/com/solovev/model/PetryPot.java
@@ -11,17 +11,29 @@ import java.util.function.Supplier;
 public class PetryPot {
     private final Bacteria[][] bacterias;
     private final int size;
+    private final LinkedList<Address> freeAddresses = new LinkedList<>();
 
-    public PetryPot() {
-        this(0);
+    /**
+     * Inner class that represent Address in the bacterias
+     *
+     * @param y array in the bacterias
+     * @param x inner index of this array in bacterias
+     */
+    private record Address(int y, int x) {
     }
 
     /**
      * Class represent response of the calculation
-     * @param days days needed to fill all the pot
+     *
+     * @param days         days needed to fill all the pot
      * @param deadBacteria dead bacteria occurred in the process
      */
-    public record Response(long days,long deadBacteria){}
+    public record Response(long days, long deadBacteria) {
+    }
+
+    public PetryPot() {
+        this(0);
+    }
 
     /**
      * Creates N*N field of empty addresses
@@ -34,6 +46,14 @@ public class PetryPot {
         }
         this.size = size;
         bacterias = new Bacteria[size][size];
+
+        //free addresses initialisation and shuffling
+        for (int i = 0; i < size; i++) {
+            for(int j =0; j< size; j++){
+                freeAddresses.add(new Address(i,j));
+            }
+        }
+        Collections.shuffle(freeAddresses);
     }
 
     /**
@@ -45,17 +65,18 @@ public class PetryPot {
     public Response calculateDays(ConfigurationOfBacteriaBehavior conf) {
         long days = 0;
         long deadBacteria = 0;
-        if(conf.toNumber() == 0){
-            return new Response(-1,0);
+        if (conf.toNumber() == 0) {
+            return new Response(-1, 0);
         }
 
-        return new Response(days,deadBacteria);
+        return new Response(days, deadBacteria);
     }
 
     /**
      * Puts random number of bacterias to the pot
      */
-    private void putBacterias(ConfigurationOfBacteriaBehavior conf){}
+    private void putBacterias(ConfigurationOfBacteriaBehavior conf) {
+    }
 
     /**
      * Goes through EMPTY neighbors of the given cell and fills it with the bacteriaSupplier based on the given supplier
@@ -72,9 +93,9 @@ public class PetryPot {
 
         for (int i = startHeight; i <= endHeight; i++) {
             for (int j = startWidth; j <= endWidth; j++) {
-                if (!(i == height && j == width) //checks it's not the same address
-                        && bacterias[i][j] == null) { //also checks for only empty addresses
-                    bacterias[i][j] = bacteriaSupplier.get();
+                if (!(i == height && j == width)) //checks it's not the same address
+                {
+                    putBacteria(i,j,bacteriaSupplier.get());
                 }
             }
         }
@@ -111,6 +132,7 @@ public class PetryPot {
         boolean placeEmpty = bacterias[height][width] == null;
         if (placeEmpty) {
             bacterias[height][width] = bacteriaToPut;
+            freeAddresses.remove(new Address(height,width));
         }
         return placeEmpty;
     }
